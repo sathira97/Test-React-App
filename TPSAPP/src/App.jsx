11b6fdda5c83466eef1f516d827016f0f579ec5e
Sathira Nimhana <sathiranimhana@gmail.com>
2025-07-23 19:22:34 +0530

    3rd Commit

--- TPSAPP/src/App.jsx
+++ TPSAPP/src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 const Card = ({ tittle }) => {
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+  useEffect(() => {
+    console.log("${tittle} has been liked: ${hasLiked}");
+  }, [hasLiked]);
+
   return (
-    <div className="card">
-      <h2>{tittle}</h2>
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>{tittle} <br/> {count}</h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "❤️" : "🤍"}
+      </button>
     </div>
   );
 };
