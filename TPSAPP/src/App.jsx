2a6aa4636ebcd776777ecea19cc6bd92006b33d2
Sathira Nimhana <sathiranimhana@gmail.com>
2025-07-25 17:12:35 +0530

    4th commit

--- TPSAPP/src/App.jsx
+++ TPSAPP/src/App.jsx
@@ -1,32 +1,23 @@
-import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-const Card = ({ tittle }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-  useEffect(() => {
-    console.log("${tittle} has been liked: ${hasLiked}");
-  }, [hasLiked]);
-
-  return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>{tittle} <br/> {count}</h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "❤️" : "🤍"}
-      </button>
-    </div>
-  );
-};
+import React from "react";
+import Search from "./components/Search";
 
 const App = () => {
   return (
-    <div className="card-container">
-      <Card tittle="Star Wars" rating={5} isCool={true} />
-      <Card tittle="Avatar" />
-      <Card tittle="The Lion King" />
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="/hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient"> Movies </span> You'll Enjoy
+            Without Hassle
+          </h1>
+        </header>
+
+        <Search />
+      </div>
+    </main>
   );
 };
 
